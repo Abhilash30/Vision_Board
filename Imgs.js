08202ac268dbcc7777972ca34ca0371aedd9f04d
Abhilash30 <Abhilashpawar14@gmail.com>
2025-08-10 04:20:29 +0530

    Initial commit

---
+++ Imgs.js
@@ -0,0 +1,61 @@
+const image_sources = [
+    "Images/6c4b00556c93154a088eeb317b6096e2.jpg",
+    //"Images/TheClimber.jpg",
+    "Images/morisan.jpg",
+    "Images/Strength.jpg",
+    "Images/sweetdecay.jpg",
+    "Images/fafka.jpg",
+    "Images/fitz.jpg",
+    "Images/Vagabond_Graphic_Design.jpg",
+    "Images/goal.jpg"
+];
+
+const img_count = 3
+const img_size = 300;
+const placed_pos = []
+
+
+function isOverlapping(x,y){
+    for (let pos of placed_pos) {
+        if (!(x + size < pos.x || x > pos.x + size || y + size < pos.y || y > pos.y + size)) {
+            return true; // overlap
+        }
+    }
+    return false;
+}
+
+for (let i = 0; i < img_count; i++) {
+    let x, y;
+    let tries = 0;
+
+    // Keep trying until we find a non-overlapping position
+    do {
+        x = Math.random() * (window.innerWidth - img_size);
+        y = Math.random() * (window.innerHeight - img_size);
+        tries++;
+    } while (isOverlapping(x, y) && tries < 100); // limit tries to avoid infinite loops
+
+    placed_pos.push({ x, y });
+
+    const img = document.createElement('img');
+    img.src = image_sources[Math.floor(Math.random() * image_sources.length)];
+    img.style.position = 'absolute';
+    img.style.width = img_size + 'px';
+    img.style.height = "auto";
+    img.style.left = x + 'px';
+    img.style.top = y + 'px';
+    document.body.appendChild(img)
+}   
+
+// for(let i = 0; i < img_count; i++){
+//     const img = document.createElement('img');
+//     img.src = image_sources[Math.floor(Math.random() * image_sources.length)];
+//     img.style.position = 'absolute';
+//     img.style.width = '300px';
+//     img.style.height = '300px';
+//     img.style.left = Math.random() * (window.innerWidth - 400) + 'px';
+//     img.style.right = Math.random() * (window.innerHeight - 400) + 'px';
+//     img.style.top = Math.random() * (window.innerHeight - 400) + 'px';
+//     document.body.appendChild(img);
+// }
+
